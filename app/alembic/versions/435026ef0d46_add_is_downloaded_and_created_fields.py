102c8b0e29dc185f48ff35dfebc2f5cbb25af1e7
Vladis <veisakov@miem.hse.ru>
2021-02-28 09:25:35 +0300

    :hammer: update: add `is_downloaded` field (for uploading)

---
+++ app/alembic/versions/435026ef0d46_add_is_downloaded_and_created_fields.py
@@ -0,0 +1,24 @@
+"""add `is_downloaded` and `created` fields
+
+Revision ID: 435026ef0d46
+Revises: 7aff3a1d2d00
+Create Date: 2021-02-18 10:56:17.262769
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '435026ef0d46'
+down_revision = '7aff3a1d2d00'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
