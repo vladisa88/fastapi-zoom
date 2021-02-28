cc922e501709a94562a1dcc3e60785d45a8088e4
Vladis <veisakov@miem.hse.ru>
2021-02-28 09:45:38 +0300

    :rocket: feat: put all logic in services

--- app/routers/zoom.py
+++ app/routers/zoom.py
@@ -1,11 +1,7 @@
 # pylint:disable=(missing-function-docstring)
-import asyncio
-
 from fastapi import APIRouter
 
 from schemas.zoom import CreateMeetingModel, ResponseMeetingModel, StopMeetingModel
-
-from models.zoom import Meeting
 
 from services.meeting import meeting_service
 from services.zoom import zoom_service
@@ -13,27 +9,21 @@ from services.zoom import zoom_service
 zoom_router = APIRouter()
 
 
-@zoom_router.post("/meeting", response_model=ResponseMeetingModel)
-async def create_meeting(data: CreateMeetingModel):
-    return await meeting_service.create(data)
-
-
 @zoom_router.get("/meetings")
 async def fetch_meetings():
     return await meeting_service.fetch_all()
 
 
-@zoom_router.delete("/meeting/{meeting_id}")
-async def stop_meeting(meeting_id: str):
-    return await zoom_service.zoom.stop_meeting(meeting_id)
+@zoom_router.post("/meeting", response_model=ResponseMeetingModel)
+async def create_meeting(data: CreateMeetingModel):
+    return await meeting_service.create(data)
 
 
 @zoom_router.put("/meetings")
 async def stop_all_meetings(data: StopMeetingModel):
-    tasks = []
-    live_meetings = await Meeting.objects.exclude(meeting_id__in=data.meetings_id).all()
-    for meeting in live_meetings:
-        task = asyncio.create_task(meeting_service.stop(meeting.meeting_id))
-        tasks.append(task)
-    await asyncio.gather(*tasks)
-    return {"status": "success"}
+    return await meeting_service.stop_list_of_meetings(data)
+
+
+@zoom_router.delete("/meeting/{meeting_id}")
+async def stop_meeting(meeting_id: str):
+    return await zoom_service.zoom.stop_meeting(meeting_id)
