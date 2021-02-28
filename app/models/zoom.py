7223d9dafb6d0b30d843bd8513b318fd8f97bc20
Vladis <veisakov@miem.hse.ru>
2021-02-28 09:25:35 +0300

    :hammer: update: add `is_downloaded` field (for uploading)

--- app/models/zoom.py
+++ app/models/zoom.py
@@ -1,4 +1,6 @@
 # pylint:disable=(invalid-name, too-few-public-methods, too-many-ancestors)
+from datetime import datetime
+
 import ormar
 from pydantic import EmailStr
 
@@ -10,6 +12,7 @@ class Meeting(ormar.Model):
     """
     Describe `Meeting` model in database
     """
+
     class Meta(BaseMeta):
         # pylint:disable=(missing-class-docstring)
         pass
@@ -22,5 +25,7 @@ class Meeting(ormar.Model):
     course_code: str = ormar.String(max_length=150, nullable=True)
     start_url: str = ormar.String(max_length=1000)
     join_url: str = ormar.String(max_length=100)
+    created: str = ormar.DateTime(default=datetime.now())
     is_active: bool = ormar.Boolean(default=True)
+    is_downloaded: bool = ormar.Boolean(default=False)
     email: EmailStr = ormar.ForeignKey(LicenseAccount)
