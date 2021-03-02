3c291bad7bad132e650544e758d1f074bca3f3eb
 vladisa88 <vladisa88@gmail.com>
2021-03-02 11:53:03 +0300

    :hammer: feat: add waiting until postgres start

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 alembic upgrade head
 uvicorn main:app --reload --port=$BACK_END_PORT --host=0.0.0.0
 
