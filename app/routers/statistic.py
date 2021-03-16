ded9865d00b41387603a0a77c27308302ac78a78
 vladisa88 <vladisa88@gmail.com>
2021-03-16 12:12:52 +0300

    :tada: feat: add new statistic api method

--- app/routers/statistic.py
+++ app/routers/statistic.py
@@ -1,11 +1,29 @@
 # pylint:disable=(missing-function-docstring)
+import typing as tp
+from datetime import date
+
 from fastapi import APIRouter
 
-from services.statistic import fetch_statistic
+from services.statistic import statistic_servive
 
 statistic_router = APIRouter()
 
 
-@statistic_router.get("/statistic")
+@statistic_router.get("/statistic/{meeting_id}")
 async def fetch_meeting_statistic(meeting_id: str):
-    return await fetch_statistic(meeting_id)
+    return await statistic_servive.fetch_statistic(meeting_id)
+
+
+@statistic_router.get("/statistic/")
+async def fetch_meeting_statistic_query(
+    date_from: date,
+    date_to: date,
+    course_code: tp.Optional[str] = None,
+    lesson_title: tp.Optional[str] = None
+):
+    return await statistic_servive.fetch_statistic_with_params(
+        date_from,
+        date_to,
+        course_code,
+        lesson_title
+    )
