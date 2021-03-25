0c8f7f48901bf3c4ea10a2cc989c640e24dd590b
 vladisa88 <vladisa88@gmail.com>
2021-03-25 16:35:31 +0300

    :toolbox: feat: add new scripts for linting and formating

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-pylint --verbose app --ignore=alembic
+pylint --verbose app tests --ignore=alembic
+black app tests --check --line-length 79
+flake8 app tests --exclude app/alembic,__init__.py
