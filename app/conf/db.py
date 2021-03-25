d44c0b99e7fc6b45a2254a082784d243efb3f5bf
 vladisa88 <vladisa88@gmail.com>
2021-03-25 16:10:47 +0300

    :tada: feat: divide on dev and prod

--- app/conf/db.py
+++ app/conf/db.py
@@ -1,10 +1,17 @@
+import os
+
 import databases
 import sqlalchemy
 
-from conf.settings import POSTGRES_CONFIG
+if not bool(os.environ["IS_TEST"]):
+    from conf.settings import POSTGRES_CONFIG
+
+    DATABASE_URL = POSTGRES_CONFIG.url
+else:
+    DATABASE_URL = "sqlite:///test.db"
 
 # settings = get_settings()
 
 metadata = sqlalchemy.MetaData()
-database = databases.Database(POSTGRES_CONFIG.url)
-engine = sqlalchemy.create_engine(POSTGRES_CONFIG.url)
+database = databases.Database(DATABASE_URL)
+engine = sqlalchemy.create_engine(DATABASE_URL)
