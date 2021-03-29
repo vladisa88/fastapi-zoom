c1ad568b29c25fce842012423ee79e1e589cf8ac
 vladisa88 <vladisa88@gmail.com>
2021-03-29 15:51:55 +0300

    :rocket: fix: add lesson information into statistic response

--- app/routers/statistic.py
+++ app/routers/statistic.py
@@ -6,21 +6,23 @@ from fastapi import APIRouter
 
 from services.statistic import statistic_servive
 
+from schemas.statistic import StatisticModel
+
 statistic_router = APIRouter()
 
 
-@statistic_router.get("/statistic/{meeting_id}")
-async def fetch_meeting_statistic(meeting_id: str):
+@statistic_router.get("/statistic/{meeting_id}", response_model=StatisticModel)
+async def fetch_meeting_statistic(meeting_id: str) -> StatisticModel:
     return await statistic_servive.fetch_statistic(meeting_id)
 
 
-@statistic_router.get("/statistic/")
+@statistic_router.get("/statistic/", response_model=list[StatisticModel])
 async def fetch_meeting_statistic_query(
     date_from: date,
     date_to: date,
     course_code: tp.Optional[str] = None,
     lesson_title: tp.Optional[str] = None,
-):
+) -> StatisticModel:
     return await statistic_servive.fetch_statistic_with_params(
         date_from, date_to, course_code, lesson_title
     )
